4da45f1d973e7615746ec1077d91a100fa3fa032
YazykovaDaria <yazykovadasva@gmail.com>
2023-03-16 12:42:15 +0300

    refactor: paths for import

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/tests/setupTests.ts",
+  },
+  resolve: {
+    alias: {
+      src: "/src/",
+    },
   },
 });
