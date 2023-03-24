eab9a8d97021ee1c06ddc2b63384176d7d0cb114
YazykovaDaria <yazykovadasva@gmail.com>
2023-03-24 12:24:06 +0300

    feat: add checkbox

--- src/components/form/AddCardForm.tsx
+++ src/components/form/AddCardForm.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import './style.css';
 import Input from './Input';
 
 const initStateErrors = {
   name: '',
   img: '',
   date: '',
+  pets: '',
+};
+
+const getInputValues = (collection: NodeListOf<HTMLInputElement>): string[] => {
+  const values: string[] = [];
+  collection.forEach((el: HTMLInputElement) => {
+    if (el.checked) {
+      values.push(el.value);
+    }
+  });
+  return values;
 };
 
 class AddCardForm extends React.Component {
@@ -14,6 +26,7 @@ class AddCardForm extends React.Component {
     this.img = React.createRef();
     this.date = React.createRef();
     this.name = React.createRef();
+    this.radio = React.createRef();
     this.state = initStateErrors;
   }
 
@@ -23,13 +36,16 @@ class AddCardForm extends React.Component {
     const file = this.img.current.files[0];
     const img = file ? URL.createObjectURL(file) : '';
     const date = this.date.current.value;
-    const isValid = this.handleValidation({ name, date, img });
+    const pets = getInputValues(this.radio.current.childNodes);
+    console.log(pets);
 
-    if (isValid) {
-      console.log(`data - ${name} ${date}`);
-      this.setState(initStateErrors);
-      event.target.reset();
-    }
+    const isValid = this.handleValidation({ name, date, img, pets });
+
+    // if (isValid) {
+    //   console.log(`data - ${name} ${date}`);
+    //   this.setState(initStateErrors);
+    //   event.target.reset();
+    // }
   };
 
   setError(key, message) {
@@ -73,6 +89,15 @@ class AddCardForm extends React.Component {
           <Input ref={this.name} label="Name" type="text" error={this.state.name}></Input>
           <Input ref={this.date} label="Date" type="date" error={this.state.date}></Input>
           <Input ref={this.img} label="Avatar" type="file" error={this.state.img}></Input>
+
+          <fieldset ref={this.radio}>
+            <p>Choose your favorite pets</p>
+            <input type="radio" id="html" name="language" value="cat"></input>
+            <label htmlFor="html">cat</label>
+            <input type="radio" id="css" name="language" value="dog"></input>
+            <label htmlFor="css">dog</label>
+            {this.state.pets ? <p className="err">{this.state.pets}</p> : null}
+          </fieldset>
 
           <div>
             <button>Submit</button>
