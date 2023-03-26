6d9564f74dc8edd11318f94bdb6b882cbc3ca7ee
YazykovaDaria <yazykovadasva@gmail.com>
2023-03-27 00:00:10 +0300

    refactor: ts and styles

--- src/components/form/AddCardForm.tsx
+++ src/components/form/AddCardForm.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './style.css';
+import { User } from 'src/types.ts/card';
 import Input from './Input';
 
-const initStateErrors = {
+type CardError = {
+  name: string;
+  img: string;
+  date: string;
+  pets: string;
+  members: string;
+  language: string;
+};
+
+interface IProps {
+  addCard: (newCard: User) => void;
+}
+
+const initStateErrors: CardError = {
   name: '',
   img: '',
   date: '',
@@ -11,85 +25,89 @@ const initStateErrors = {
   language: '',
 };
 
-const getInputValues = (collection: NodeListOf<HTMLInputElement>, type: string): string[] => {
+const getInputValues = (collection: NodeListOf<ChildNode>, type: string): string[] => {
   const values: string[] = [];
-  collection.forEach((el: HTMLInputElement) => {
-    if (el.checked) {
-      const value = type === 'radio' ? el.value : el.name;
-      values.push(value);
+  collection.forEach((el) => {
+    if (el instanceof HTMLInputElement) {
+      if (el.checked) {
+        const value = type === 'radio' ? el.value : el.name;
+        values.push(value);
+      }
     }
   });
   return values;
 };
 
-class AddCardForm extends React.Component {
-  constructor(props) {
-    super(props);
+class AddCardForm extends React.Component<IProps, CardError> {
+  private img = React.createRef<HTMLInputElement>();
+  private date = React.createRef<HTMLInputElement>();
+  private name = React.createRef<HTMLInputElement>();
+  private radio = React.createRef<HTMLFieldSetElement>();
+  private checkbox = React.createRef<HTMLFieldSetElement>();
+  private select = React.createRef<HTMLSelectElement>();
 
-    this.img = React.createRef();
-    this.date = React.createRef();
-    this.name = React.createRef();
-    this.radio = React.createRef();
-    this.checkbox = React.createRef();
-    this.select = React.createRef();
+  constructor(props: IProps) {
+    super(props);
     this.state = initStateErrors;
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const name = this.name.current.value;
-    const file = this.img.current.files[0];
+    const name = this.name.current!.value;
+    const file = this.img.current!.files![0];
     const img = file ? URL.createObjectURL(file) : '';
-    const date = this.date.current.value;
-    const pets = getInputValues(this.radio.current.childNodes, 'radio');
-    const members = getInputValues(this.checkbox.current.childNodes, 'checkbox');
-    const language = this.select.current.value;
+    const date = this.date.current!.value;
+    const pets = getInputValues(this.radio.current!.childNodes, 'radio');
+    const members = getInputValues(this.checkbox.current!.childNodes, 'checkbox');
+    const language = this.select.current!.value;
 
-    const isValid = this.handleValidation({ name, date, img, pets, members, language });
+    const newCard = { name, date, img, pets, language, members };
+    this.handleValidation(newCard);
+    const isValid = this.handleValidation(newCard);
+    console.log(isValid);
 
-    // if (isValid) {
-    //   console.log(`data - ${name} ${date}`);
-    //   this.setState(initStateErrors);
-    //   event.target.reset();
-    // }
+    if (isValid) {
+      this.props.addCard(newCard);
+      this.setState(initStateErrors);
+      event.currentTarget.reset();
+      alert('Card was added');
+    }
   };
 
-  setError(key, message) {
+  setError(key: keyof CardError, message: string) {
     this.setState({
       [key]: message,
-    });
+    } as Pick<CardError, keyof CardError>);
   }
 
-  handleValidation = (data): boolean => {
-    let flag = true;
-    const entries = Object.entries(data);
-    //console.log(entries);
+  handleValidation = (data: User): boolean => {
+    const entries: [string, string[] | string][] = Object.entries(data);
+    const errors: string[] = [];
 
-    entries.forEach(([key, val]) => {
-      //console.log(val.length);
-
+    entries.forEach(([stringKey, val]) => {
+      const key = stringKey as keyof CardError;
       if (val.length > 0) {
-        if (key === 'name') {
+        if (key === 'name' && typeof val === 'string') {
           const char = val.charAt(0);
-          flag = char.toUpperCase() === char;
-          const message = flag ? '' : 'be big';
+          const message =
+            char.toUpperCase() === char ? '' : 'name must start with a capital letter';
           this.setError(key, message);
+          errors.push(message);
         } else {
-          flag = true;
           this.setError(key, '');
         }
       } else {
         this.setError(key, 'requaried');
-        flag = false;
+        errors.push('err');
       }
     });
-
-    return flag;
+    const isValid = errors.filter((val) => val !== '');
+    return isValid.length < 1;
   };
 
   render() {
     return (
-      <div>
+      <div data-testid="add-card-form">
         <h2 className="title">Add user card</h2>
         <form onSubmit={this.handleSubmit} className="form">
           <Input ref={this.name} label="Name" type="text" error={this.state.name}></Input>
@@ -98,8 +116,8 @@ class AddCardForm extends React.Component {
 
           <fieldset ref={this.radio}>
             <p className="form-title">Is cats cool?</p>
-            <input type="radio" id="html" name="language" value="yes"></input>
-            <label htmlFor="html">yes</label>
+            <input type="radio" id="html" name="language" value="of course"></input>
+            <label htmlFor="html">of course</label>
             <input type="radio" id="css" name="language" value="absolutely"></input>
             <label htmlFor="css">absolutely</label>
             {this.state.pets ? <p className="err">{this.state.pets}</p> : null}
