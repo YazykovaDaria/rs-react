3f3d83aa3296c88e7ba3cb8149accb63f6e61346
YazykovaDaria <yazykovadasva@gmail.com>
2023-04-07 17:26:04 +0300

    feat: response

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Search from 'src/components/search/Search';
 import getSavedSearchVal from 'src/utils/utils';
 import Cards from 'src/components/cards/Cards';
-import data from 'src/tests/cardsData';
+import { getCharacters } from 'src/utils/utils';
+import Card from 'src/types/card';
 
 function Main() {
+  const [data, setData] = useState<Card[]>([]);
+  const [isLoading, setLoading] = useState(false);
+  const savedSearchVal = getSavedSearchVal();
+
+  const getSearchUrl = (url: string) => `/?name=${url}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCharacters(getSearchUrl(savedSearchVal));
+      setData(data);
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const changeData = async (url: string) => {
+    setLoading(true);
+    const data = await getCharacters(getSearchUrl(url));
+    setLoading(false);
+    setData(data);
+  };
+
   return (
     <div className="wrapper">
-      <Search value={getSavedSearchVal()} />
-      <Cards cards={data} />
+      <Search value={getSavedSearchVal()} onSubmit={changeData} />
+      <div>
+        {isLoading && <p>Loading</p>}
+        {data.length === 0 ? (
+          <p>Nothing was found for your request</p>
+        ) : (
+          <Cards cards={data}></Cards>
+        )}
+      </div>
     </div>
   );
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,23 @@
+import Card from 'src/types/card';
+
 function getSavedSearchVal(): string {
   const savedVal = localStorage.getItem('search');
   return savedVal || '';
 }
 
 export default getSavedSearchVal;
+
+export const getCharacters = async (url = ''): Promise<Card[]> => {
+  const baseUrl = 'https://rickandmortyapi.com/api/character';
+  try {
+    const response = await fetch(`${baseUrl}${url}`);
+    const data = await response.json();
+    if (data.error) {
+      return [];
+    }
+    return data.results;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
