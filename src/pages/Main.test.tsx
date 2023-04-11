bda4188caf9110d55d282a0f199fa4582e7bdefa
YazykovaDaria <yazykovadasva@gmail.com>
2023-04-11 17:10:22 +0300

    refactor: test

--- src/pages/Main.test.tsx
+++ src/pages/Main.test.tsx
@@ -1,44 +1,15 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
 import { screen } from '@testing-library/react';
-import { beforeAll, afterAll } from 'vitest';
 import Main from './Main';
-import data from 'src/tests/cardsData';
-
-const server = setupServer(
-  rest.get('https://rickandmortyapi.com/api/character', (_req, res, ctx) => {
-    return res(ctx.json({ results: data }));
-  })
-);
 
 describe('Main', () => {
-  beforeAll(async () => {
-    await server.listen();
-  });
-
-  afterAll(async () => {
-    await server.close();
-  });
-
-  it('renders search bar', async () => {
+  it('renders search bar and cards correctly', async () => {
     const { getByTestId } = render(<Main />);
     const searchBar = getByTestId('search-bar');
     expect(searchBar).toBeInTheDocument();
-  });
-
-  it('renders error message when data not found', async () => {
-    render(<Main />);
-
-    server.use(
-      rest.get('https://rickandmortyapi.com/api/character/name=;', (_req, res, ctx) => {
-        return res(ctx.json({ results: [] }));
-      })
-    );
-
-    const message = await screen.findByText('Nothing was found for your request');
-    expect(message).toBeInTheDocument();
+    const cards = await screen.findByTestId('cards');
+    expect(cards).toBeInTheDocument();
   });
 
   it('displays spinner while loading', async () => {
