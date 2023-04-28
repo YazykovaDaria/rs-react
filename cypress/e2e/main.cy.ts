0a62920295636b06d4effad73f2eb99e4c3ba443
YazykovaDaria <yazykovadasva@gmail.com>
2023-04-28 21:10:50 +0300

    feat: test for main page

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -2,9 +2,24 @@ describe('Main page', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.get(`[data-testid=card]`).as('cards');
+    cy.get('input[type="search"]').as('search');
+    cy.get(`[data-testid=search-bar]`).as('form');
   });
 
-  it('renders cards', () => {
+  it('render cards', () => {
     cy.get('@cards').should('have.length', 20);
+  });
+
+  it('search cards with valid value', () => {
+    cy.get('@search').type('for');
+    cy.get(`@form`).submit();
+    cy.get('@cards').should('have.length', 5);
+  });
+
+  it('show error message when invalid search value', () => {
+    cy.get('@search').type('arad');
+    cy.get('@form').submit();
+
+    cy.contains('Nothing was found for yor request');
   });
 });
